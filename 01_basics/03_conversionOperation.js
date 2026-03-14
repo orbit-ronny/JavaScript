b6f4e563ceb719b11be190ee7d8d55a6d5589bcf
orbit-ronny <rohan99301@gmail.com>
2026-03-14 12:29:24 +0530

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "rohan"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN not an number
+// true => 1, false => 0
+
+let isloggedIn = "rohan"
+
+let booleanIsLoggedIn = Boolean(isloggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// " => false
+// "rohan" => true
+
+let someNumber = 33
+
+let stringNubmer = String(someNumber)
+console.log(typeof stringNubmer);
