3c8e7d626cf558a34f401a26c9592c91f38396b9
orbit-ronny <rohan99301@gmail.com>
2026-03-18 16:14:44 +0530

    Scope level and mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,9 +3,57 @@ let a = 300
 if (true) {
     let a = 10
     const b = 20
-    console.log("INNER: ", a);    
+    // console.log("INNER: ", a);    
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+// Nested Scope
+
+function one(){
+    const username = "rohan"
+
+    function two(){
+        const website = "YouTube"
+        // console.log(username);
+    }
+    // console.log(website);
+    
+    two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "rohan"
+    if (username === "rohan") {
+        const website = " YouTube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+
+// ++++++++++++++++++++++  interesting ++++++++++++++++++++++++
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+
+}
+
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
